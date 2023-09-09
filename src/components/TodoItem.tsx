e61417923296e64eec05b0ed87caa49c0d00a9ea
jeongseoe <gldi1633@naver.com>
2023-09-09 16:04:00 +0900

    fix : 일단 id 값 없어도되서 주석처리함

---
+++ src/components/TodoItem.tsx
@@ -0,0 +1,82 @@
+import React from "react";
+import styled, { css } from "styled-components";
+import { MdDone, MdDelete } from "react-icons/md";
+
+const Remove = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #dee2e6;
+  font-size: 24px;
+  cursor: pointer;
+  &:hover {
+    color: #ff6b6b;
+  }
+  display: none;
+`;
+
+const TodoItemBlock = styled.div`
+  display: flex;
+  align-items: center;
+  padding-top: 12px;
+  padding-bottom: 12px;
+  &:hover {
+    ${Remove} {
+      display: initial;
+    }
+  }
+`;
+
+// styled 에 props를 사용할 때도 타입을 지정해야한다.
+const CheckCircle = styled.div<{ done: boolean }>`
+  width: 32px;
+  height: 32px;
+  border-radius: 16px;
+  border: 1px solid #ced4da;
+  font-size: 24px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-right: 20px;
+  cursor: pointer;
+  ${(props) =>
+    props.done &&
+    css`
+      border: 1px solid #38d9a9;
+      color: #38d9a9;
+    `}
+`;
+
+const Text = styled.div<{ done: boolean }>`
+  flex: 1;
+  font-size: 21px;
+  color: #495057;
+  ${(props) =>
+    props.done &&
+    css`
+      color: #ced4da;
+    `}
+`;
+
+interface Props {
+  done: boolean;
+  text: string;
+  // id: number;
+}
+
+function TodoItem(props: Props) {
+  const { done, text } = props;
+
+  // Todo에 action을 부여하기 위한 dispatch와 eventHandler
+  return (
+    <TodoItemBlock>
+      <CheckCircle done={done}>{done && <MdDone />}</CheckCircle>
+      <Text done={done}>{text}</Text>
+      <Remove>
+        <MdDelete />
+      </Remove>
+    </TodoItemBlock>
+  );
+}
+
+export default TodoItem;
