b2873f0258fb6488a5c0da4a58ce40a467b2d670
jeongseoe <gldi1633@naver.com>
2023-10-29 15:13:51 +0900

    feat : 카테고리마다 나눠지게 변경함

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -6,7 +6,7 @@ import { useTodoState } from "./TodoContext";
 const TodoListBlock = styled.div`
   flex: 1;
   padding: 20px 32px;
-  padding-bottom: 48px;
+  padding-bottom: 290px;
   overflow: scroll;
 `;
 
@@ -18,17 +18,45 @@ function TodoList(props: Props) {
   const { isDark } = props;
   const todos = useTodoState();
 
+  const 카테고리 = todos.map((a) => a.category);
+  const 중복제거 = 카테고리.filter((v, i) => 카테고리.indexOf(v) === i);
+
   return (
     <TodoListBlock>
-      {todos.map((todo) => (
-        <TodoItem
-          isDark={isDark}
-          key={todo.id}
-          id={todo.id}
-          text={todo.text}
-          isDone={todo.isDone}
-        />
-      ))}
+      {중복제거.map((title) => {
+        return (
+          <>
+            <div style={{ paddingBottom: "20px", paddingTop: "10px" }}>
+              {title}
+            </div>
+            {todos.map((item) => {
+              if (item.category === `${title}`) {
+                return (
+                  <TodoItem
+                    isDark={isDark}
+                    key={item.id}
+                    id={item.id}
+                    text={item.text}
+                    isDone={item.isDone}
+                  />
+                );
+              }
+            })}
+          </>
+        );
+      })}
+      {/* {todos.map((todo)=>{
+        return (
+          <TodoItem
+            isDark={isDark}
+            category={todo.category}
+            key={todo.id}
+            id={todo.id}
+            text={todo.text}
+            isDone={todo.isDone}
+          />
+        );
+      })} */}
     </TodoListBlock>
   );
 }
